c9f13b7735eaad14155fc4102ab7dcbddfda998c
Guilherme Felipe <glipecoelho2@gmail.com>
2024-12-27 15:03:53 -0300

    feat: organization and fixed some errors

--- price_checker/main.py
+++ price_checker/main.py
@@ -1,10 +1,29 @@
 import pandas as pd
 from db import inicializar_banco
 from ui import ui_c
+from scraping import extrair_dados_magalu
 
-class PriceChecker:
+class Main:
+    def __init__(self):
+        self.path = None
+        self.app = ui_c()
+
     inicializar_banco()
+
+    def analisar_preco(self):
+        resultado = scrap
+        print(resultado)
+    
+
+    def get_data():
+        get = pd.read_excel()
+
+    def path_get(self):
+        self.path = self.app.locale_path()
 
 if __name__ == "__main__":
     app = ui_c()
     app.inicializar_ui()
+
+url = "https://www.magazineluiza.com.br/bebida-lactea-uht-com-15g-de-proteinas-yopro-chocolate-sem-lactose-zero-acucar-250ml/p/226969600/me/belc/"
+scrap = extrair_dados_magalu(url)
--- price_checker/ui.py
+++ price_checker/ui.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-from scraping import extrair_dados_magalu
 
 class ui_c:
     def __init__(self):
@@ -23,7 +22,7 @@ class ui_c:
         label_titulo = tk.Label(self.root, text="Analisador de Preços", font=("Arial", 16, "bold"))
         label_titulo.pack(pady=10)
         
-        self.localizar_button = tk.Button(self.root, text = "Selecionar arquivo XLSX", command=self.locale)
+        self.localizar_button = tk.Button(self.root, text = "Selecionar arquivo XLSX", command=self.locale_path)
         self.localizar_button.pack(pady=10)
 
         self.caminho_arquivo = tk.StringVar()
@@ -36,18 +35,16 @@ class ui_c:
         self.result_label = tk.Label(self.root, text="", font=("Arial", 12), justify="left", wraplength=380)
         self.result_label.pack(pady=10)
 
-    def locale(self):
+    def locale_path(self):
         tipos_arquivos = [("Arquivos CSV e Excel", "*.csv;*.xlsx")]
         filename = askopenfilename(title="Selecionar Arquivo",filetypes=tipos_arquivos)
-        print(filename)    
+        print(filename)
         if filename:
             self.caminho_arquivo.set(filename)
-
+        return filename
+    
     def analisar_preco(self):
-        url = "https://www.magazineluiza.com.br/bebida-lactea-uht-com-15g-de-proteinas-yopro-morango-sem-lactose-zero-acucar-250ml/p/234133400/me/bebp/"
-        resultado = self.extrair_dados_magalu(url)
-        print(resultado)
-        self.result_label.config(text=resultado)
+        self.result_label.config(text="Concluido!")
 
     def inicializar_ui(self):
         self.root.mainloop()
